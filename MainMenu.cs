48ffbd6ec7d3cd56c62b699d6fabb8533304481c
Victor Alvizo <37986878+VictorAlvizo@users.noreply.github.com>
2018-04-01 17:05:34 -0500

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        SoundManager.instance.PlayOneShot(SoundManager.instance.coryHurt);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CreditButton()
+    {
+        SoundManager.instance.PlayOneShot(SoundManager.instance.coryHurt);
+    }
+
+    public void QuitGame()
+    {
+        SoundManager.instance.PlayOneShot(SoundManager.instance.coryHurt);
+        Application.Quit();
+    }
+}
