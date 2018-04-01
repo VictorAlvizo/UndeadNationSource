cd460fafaa7183054745f0d2282dbfee8ed7a9bb
Victor Alvizo <37986878+VictorAlvizo@users.noreply.github.com>
2018-04-01 17:03:51 -0500

    Add files via upload

---
+++ EnemBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemBullet : MonoBehaviour {
+
+    public float speed = 8f;
+	
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Bullet")
+        {
+            Destroy(gameObject);
+            Destroy(col.gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
