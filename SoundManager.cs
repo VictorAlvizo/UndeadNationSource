e52c0bc2b3b167105caf23e54eeeb9d82b5ead67
Victor Alvizo <37986878+VictorAlvizo@users.noreply.github.com>
2018-04-01 17:06:07 -0500

    Add files via upload

---
+++ SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance = null;
+
+    public AudioClip zombieGrunt;
+    public AudioClip damageTaken;
+    public AudioClip pickUp;
+    public AudioClip bulletFire;
+    public AudioClip reload;
+    public AudioClip emptyGun;
+    public AudioClip itemDrop;
+    public AudioClip moreHealth;
+    public AudioClip gameOver;
+    public AudioClip coryHurt;
+
+    private AudioSource soundEffectAudio;
+
+	void Start () {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        AudioSource theSource = GetComponent<AudioSource>();
+        soundEffectAudio = theSource;
+	}
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+}
