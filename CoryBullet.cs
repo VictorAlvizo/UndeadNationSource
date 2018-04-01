76cf7018c2566c5bcfa028322f2337ea1118dcc5
Victor Alvizo <37986878+VictorAlvizo@users.noreply.github.com>
2018-04-01 17:03:11 -0500

    Add files via upload

---
+++ CoryBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoryBullet : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        Vector2 dir = GameObject.Find("Cory(Clone)").GetComponent<CoryAI>().bulletDirection;
+
+        transform.up = dir;
+
+        rb.velocity = dir * speed;
+	}
+	
+	void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
