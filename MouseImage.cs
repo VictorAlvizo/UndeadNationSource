905c00781f9c622c9deea4efc0437be5bec8e25c
Victor Alvizo <37986878+VictorAlvizo@users.noreply.github.com>
2018-04-01 17:07:53 -0500

    Add files via upload

---
+++ MouseImage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseImage : MonoBehaviour {
+
+    public Texture2D targetCursor;
+
+    private CursorMode curMode = CursorMode.Auto;
+    private Vector2 hotSpot = Vector2.zero;
+
+	void Start () {
+        Cursor.SetCursor(targetCursor, hotSpot, curMode);
+	}
+}
