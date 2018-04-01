fe20ae9dab8f39d1a044f0d04198112f5472d973
Victor Alvizo <37986878+VictorAlvizo@users.noreply.github.com>
2018-04-01 17:00:10 -0500

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 4f;
+
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        Vector2 direction = GameObject.Find("Player").GetComponent<Player>().bulletDirection;
+
+        rb.velocity = direction * speed;
+
+        transform.up = direction;
+	}
+	
+	void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
